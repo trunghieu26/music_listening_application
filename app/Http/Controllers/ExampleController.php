3a46925a5f8a9265115b59bb93cc98474697da3a
trunghieu26 <trunghieuqt2610@gmail.com>
2024-03-22 13:23:13 +0700

    update code example

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\ApiResponse\ApiResponse;
+
+class ExampleController extends Controller
+{
+    use ApiResponse;
+
+    /**
+     * Get list User
+     *
+     * @return void
+     */
+    public function index()
+    {   
+        return $this->successResponse("", ["1", "2"]);
+    }
+    
+}
